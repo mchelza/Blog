e61b3a8f98f5678fb65f7df5964d8a89723902af
Chella M <mchelza@gmail.com>
2011-10-12 17:33:42 +0530

    NEW changes

--- Prog blog/Prog blog/main.m
+++ Prog blog/Prog blog/main.m
@@ -7,14 +7,67 @@
 //
 
 // simple fraction
+
 #import<Foundation/Foundation.h>
+
+//----@ Interface section------
+
+@interface Fraction : NSObject 
+{
+    int numerator;
+    int denomenator;
+}
+
+-(void) print;
+-(void) setnumerator: (int) n;
+-(void) setdenomenator: (int) d;
+
+@end
+
+//---- @ Implementatio Section ----
+
+@implementation Fraction
+-(void) print
+{
+    NSLog(@"%i/%i",numerator , denomenator);
+}
+
+-(void) setnumerator:(int)n
+{
+    numerator = n;
+}
+
+-(void) setdenomenator:(int)d
+{
+    denomenator = d;
+}
+
+@end
+
+//-----Program section------
 
 int main (int argc, char *argv[])
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-    int numerator = 3;
-    int denomenator = 4;
-    NSLog(@"The fraction is %i/%i" ,numerator,denomenator);
+    Fraction *myfraction;
+    
+    // create a intsance of Fraction
+    
+    myfraction = [Fraction alloc];
+    myfraction = [myfraction init];
+    
+    // set fraction to 1/3
+    
+    [myfraction setnumerator:1];
+    [myfraction setdenomenator:3];
+    
+    // display the fraction using the print method
+    
+    NSLog(@"The Fraction is:");
+    [myfraction print];
+    [myfraction release];
+    
+    
     [pool drain];
     return 0;
     
