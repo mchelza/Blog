88e701c1decd4fd7944eb1f6d05b5326a9cd4f63
Chella M <mchelza@gmail.com>
2011-10-12 11:20:42 +0530

    2nd commit

---
+++ Prog blog/Prog blog/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  Prog blog
+//
+//  Created by CHELLA M on 12/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+    // insert code here...
+    NSLog(@"Hello, World!");
+    NSLog(@"Testing");
+
+    [pool drain];
+    return 0;
+}
+
